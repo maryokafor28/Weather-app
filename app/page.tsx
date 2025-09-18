74995dd5ca6b94a39ebbe5444a933a945e374b94
maryokafor28 <vincyokafor@gmail.com>
2025-09-18 08:39:12 +0100

    suspense import

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from "react";
+
 import Topbar from "@/components/common/Topbar";
 import Search from "@/components/common/SearchInput";
 import WeatherPage from "@/components/common/Weather";
@@ -18,7 +20,9 @@ export default function Dashboard() {
       <div className="max-w-7xl mx-auto mt-8 flex flex-col lg:flex-row lg:space-x-4 lg:pl-12">
         {/* Left column */}
         <div className="flex-1 space-y-2 w-full lg:max-w-3xl">
-          <WeatherPage />
+          <Suspense fallback={<p className="text-white">Loading weather...</p>}>
+            <WeatherPage />
+          </Suspense>
           <WeatherStats />
           <ForecastPage />
         </div>
